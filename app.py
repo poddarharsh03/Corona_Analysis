30b650723e4ff53fc834c8b11bdeeecbf32fb014
poddarharsh03 <harshp1605@gmail.com>
2020-07-16 22:21:05 +0530

    hello

--- app.py
+++ app.py
@@ -29,13 +29,38 @@ reader = csv.reader(open("state.csv", 'r'))
 d={}
 
 for row in reader:
-    d[row[0]]= {"name": row[1],"total": row[2],"cured": row[3],"death": row[4]}
+    d[row[0]]= {"name": row[1],"total": int(row[2]),"cured": int(row[3]),"death": int(row[4])}
 print(d)
 
 def graph_plt(z):
     s=str(z)
     a=d[s]
-    nm,tot,cur,deat=a['name'],a['total'],a['cured'],a['death']
-    print(nm,tot,cur,deat)
+    nm,total,cured,death=a['name'],a['total'],a['cured'],a['death']
+    infected=total-cured-death
+    print(nm,total,cured,death)
+    # --- dataset 1: just 4 values for 4 groups:
+    fig, ax = plt.subplots()
+    plt.rcParams['font.sans-serif'] = 'Arial'
+    plt.rcParams['font.family'] = 'sans-serif'
+    plt.rcParams['text.color'] = '#909090'
+    plt.rcParams['axes.labelcolor']= '#909090'
+    plt.rcParams['xtick.color'] = '#909090'
+    plt.rcParams['ytick.color'] = '#909090'
+    plt.rcParams['font.size']=12
+    plt.title(nm)
+    labels = ['Dead', 
+            'Cured', 'infected']
+    percentages = [death*100/total, cured*100/total,infected*100/total]
+    explode=(0.1,0,0)
+    ax.pie(percentages, explode=explode, labels=labels,  
+         autopct='%1.0f%%', 
+        shadow=False, startangle=0,   
+        pctdistance=1.2,labeldistance=1.4)
 
+    ax.legend(frameon=False, bbox_to_anchor=(1.5,0.8))
+
+ 
+    
+    plt.show()
+graph_plt(20)
 
